168b49e668f5aaa51bcf1453c8d2420430e99703
Vadim Gartman <vadim.gartman@yandex.ru>
2022-04-17 12:27:04 +0300

    backend: add celery task

---
+++ backend/event/tasks.py
@@ -0,0 +1,7 @@
+from django.core.management import call_command
+from main.celery import app
+
+
+@app.task(name='Import events')
+def import_events(*args, **kwargs):
+    return call_command('import_events', verbosity=0)
